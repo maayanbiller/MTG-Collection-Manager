7925271705dfd6cdd56e04efc50b396e7b51f353
maayanbiller <maayan.biller@gmail.com>
2022-05-25 23:00:10 +0300

    get price and image by card name

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print('hi there')
+import urllib.request
+import urllib.parse
+import re
+
+card_name = input('enter a card name: ')
+
+webUrl = 'https://scryfall.com/search?q=!%22{}%22'.format(card_name)
+# webUrl = 'https://scryfall.com/search?q=!%22sakashima%20of%20a%20thousand%20faces%22'
+# webUrl = 'https://scryfall.com/card/cmr/89/sakashima-of-a-thousand-faces'
+HTML = urllib.request.urlopen(webUrl).read()
+
+
+img_url = re.search(r"img.*?src=\"(?P<img_url>[^\"]*)\"", HTML.decode("utf-8"), re.DOTALL)['img_url']
+price = re.search(r"<a title=\"Nonfoil: \$(?P<price>[^,]*)[^>]*>", HTML.decode("utf-8"), re.DOTALL)['price']
+print(f'img url : {img_url}\nnonfoil price : {price}')
+
+# print(HTML)
+# content = re.search(r"<div id=\"mw-content-text.*id=\"catlinks", HTML.decode("utf-8"), re.DOTALL)
+# print(content)
